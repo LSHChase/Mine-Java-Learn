0a36a8875c8afa2fbc7376521c5b97db97d59ba8
sx-9524 <sx-liush@dtdream.com>
2018-11-27 11:32:44 +0800

    添加启动

---
+++ testReadJava/src/main/java/com/me/note/start.java
@@ -0,0 +1,22 @@
+/*
+ * ------------------------------------------------------------------
+ * Copyright © 2018 DtDream Science and Technology Co.,Ltd. All rights reserved.
+ * ------------------------------------------------------------------
+ *       Product: ASR
+ *   Module Name: alarm-rule
+ *  Date Created: 2018-11-27
+ *   Description:
+ * ------------------------------------------------------------------
+ * Modification History
+ * DATE            Name           Description
+ * ------------------------------------------------------------------
+ * 2018-11-27      sx-9524
+ * ------------------------------------------------------------------
+ */
+package com.me.note;
+
+public class start {
+    public static void main(String[] args) {
+        System.out.println("Run test read java SUCCESS!");
+    }
+}
